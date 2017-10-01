4022b98ada4bb22af4dad858ee57f454f58f3892
Michael Dunlap <sagaquisces@gmail.com>
2017-09-30 22:45:50 -0700

    upon logout from main menu in main activity, user will be logged out of firebase and redirected to StartActivity

--- app/src/main/java/com/epicodus/classicalchat/MainActivity.java
+++ app/src/main/java/com/epicodus/classicalchat/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -36,9 +38,32 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         if(currentUser == null) {
-            Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
-            startActivity(startIntent);
-            finish();
+            sendToStart();
         }
+    }
+
+    private void sendToStart() {
+        Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(startIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.mainLogoutBtn) {
+            FirebaseAuth.getInstance().signOut();
+            sendToStart();
+
+        }
+        return true;
     }
 }
