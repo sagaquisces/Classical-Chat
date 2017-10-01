1ca56df146f2aa7b83a5df43e51b80f311c67919
Michael Dunlap <sagaquisces@gmail.com>
2017-09-30 20:10:00 -0700

    regist button on start activity will route to register activity.

--- app/src/main/java/com/epicodus/classicalchat/StartActivity.java
+++ app/src/main/java/com/epicodus/classicalchat/StartActivity.java
@@ -1,13 +1,29 @@
 package com.epicodus.classicalchat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button mRegBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        mRegBtn = (Button) findViewById(R.id.startRegBtn);
+
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent reg_intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(reg_intent);
+            }
+        });
     }
 }
