1a0bcc809c533ce7eec2b6a280d1f32053c1f94a
Michael Dunlap <sagaquisces@gmail.com>
2017-09-30 20:20:47 -0700

    refactor StartActivity to implement onClickListener and ButterKnife binding

--- app/src/main/java/com/epicodus/classicalchat/StartActivity.java
+++ app/src/main/java/com/epicodus/classicalchat/StartActivity.java
@@ -6,24 +6,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-    private Button mRegBtn;
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.startRegBtn) Button mRegBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        mRegBtn = (Button) findViewById(R.id.startRegBtn);
+        ButterKnife.bind(this);
 
-        mRegBtn.setOnClickListener(new View.OnClickListener() {
+        mRegBtn.setOnClickListener(this);
+    }
 
-            @Override
-            public void onClick(View view) {
-                Intent reg_intent = new Intent(StartActivity.this, RegisterActivity.class);
-                startActivity(reg_intent);
-            }
-        });
+    @Override
+    public void onClick(View view) {
+        if(view == mRegBtn) {
+            Intent reg_intent = new Intent(StartActivity.this, RegisterActivity.class);
+            startActivity(reg_intent);
+        }
     }
 }
