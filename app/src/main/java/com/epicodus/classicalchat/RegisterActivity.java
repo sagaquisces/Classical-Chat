d9cc568adbc3ca5a7990d761684a3da71f4b3318
Michael Dunlap <sagaquisces@gmail.com>
2017-10-01 20:39:46 -0700

    add progress dialog for Register Activity

--- app/src/main/java/com/epicodus/classicalchat/RegisterActivity.java
+++ app/src/main/java/com/epicodus/classicalchat/RegisterActivity.java
@@ -1,11 +1,13 @@
 package com.epicodus.classicalchat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -29,6 +31,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     @Bind(R.id.regCreateBtn) Button mCreateBtn;
 
+    private ProgressDialog mRegProgress;
     private FirebaseAuth mAuth;
 
     @Override
@@ -41,6 +44,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Create Account");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mRegProgress = new ProgressDialog(this);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -60,7 +65,17 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         boolean validPassword = isValidPassword(password, confirm_password);
         if (!validEmail || !validName || !validPassword) return;
 
-        register_user(display_name, email, password, confirm_password);
+        if(!TextUtils.isEmpty(display_name) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
+
+            mRegProgress.setTitle("Registering User");
+            mRegProgress.setMessage("Please wait while we create your account.");
+            mRegProgress.setCanceledOnTouchOutside(false);
+            mRegProgress.show();
+
+            register_user(display_name, email, password, confirm_password);
+        }
+
+
     }
 
     private void register_user(String display_name, String email, String password, String confirm_password) {
@@ -68,11 +83,16 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()) {
+
+                    mRegProgress.dismiss();
+
                     Intent main_intent = new Intent (RegisterActivity.this, MainActivity.class);
                     startActivity(main_intent);
                     finish();
                 } else {
-                    Toast.makeText(RegisterActivity.this, "You got an error", Toast.LENGTH_LONG).show();
+
+                    mRegProgress.hide();
+                    Toast.makeText(RegisterActivity.this, "Cannot sign in. Please check the form and try again.", Toast.LENGTH_LONG).show();
                 }
             }
         });
