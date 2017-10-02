7d5c6822d0d4641dbd3a78a9f9b5bbb67a2fe24c
Guest User <Guest@epicodus-3.local>
2017-10-02 09:26:47 -0700

    create basic tab layout for chat feature in ChatActivity

--- app/src/main/java/com/epicodus/classicalchat/ChatActivity.java
+++ app/src/main/java/com/epicodus/classicalchat/ChatActivity.java
@@ -1,13 +1,36 @@
 package com.epicodus.classicalchat;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ChatActivity extends AppCompatActivity {
+
+    @Bind(R.id.chat_toolbar) Toolbar mToolbar;
+    @Bind(R.id.chat_tabPager) ViewPager mViewPager;
+    @Bind(R.id.chat_tabs) TabLayout mTabLayout;
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Classical Chat");
+
+        //Tabs
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 }
---
+++ app/src/main/java/com/epicodus/classicalchat/SectionsPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.epicodus.classicalchat;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Guest on 10/2/17.
+ */
+
+class SectionsPagerAdapter extends FragmentPagerAdapter{
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch(position) {
+            case 0:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return requestsFragment;
+
+            case 1:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+
+            case 2:
+                FriendsFragment friendsFragment = new FriendsFragment();
+                return friendsFragment;
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
