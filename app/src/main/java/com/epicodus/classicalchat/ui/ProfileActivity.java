56c0e8ce2a3f397dddd844a8882afdd52a6a7779
Michael Dunlap <sagaquisces@gmail.com>
2017-10-09 16:57:45 -0700

    users can send friend requests and db will reflect sent and received by using user ids.

--- app/src/main/java/com/epicodus/classicalchat/ui/ProfileActivity.java
+++ app/src/main/java/com/epicodus/classicalchat/ui/ProfileActivity.java
@@ -1,18 +1,49 @@
 package com.epicodus.classicalchat.ui;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.classicalchat.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @Bind(R.id.profile_name)
-    TextView mID;
+    @Bind(R.id.profile_image) ImageView mProfileImageView;
+    @Bind(R.id.profile_name) TextView mProfileName;
+    @Bind(R.id.profile_status) TextView mProfileStatus;
+    @Bind(R.id.profile_total_friends) TextView mProfileFriendsCount;
+    @Bind(R.id.profile_send_req_btn) Button mProfileSendReqBtn;
+
+    private DatabaseReference mUsersDatabase;
+
+    private ProgressDialog mProgress;
+
+    private FirebaseUser mCurrentUser;
+
+    private DatabaseReference mFriendReqDatabase;
+
+    private String mUserProfileID;
+    private String mCurrent_state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +52,81 @@ public class ProfileActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        String user_id = getIntent().getStringExtra("user_id");
+        mUserProfileID = getIntent().getStringExtra("user_id");
 
-        mID.setText(user_id);
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(mUserProfileID);
+        mFriendReqDatabase = FirebaseDatabase.getInstance().getReference().child("Friend_req");
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        mCurrent_state = "not_friends";
+
+        mProgress = new ProgressDialog(this);
+        mProgress.setTitle("Loading User Data");
+        mProgress.setMessage("Please wait while we load the user data");
+        mProgress.setCanceledOnTouchOutside(false);
+        mProgress.show();
+
+        mProfileSendReqBtn.setOnClickListener(this);
+
+        mUsersDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("name").getValue().toString();
+                String status = dataSnapshot.child("status").getKey().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+
+                mProfileName.setText(name);
+                mProfileStatus.setText(status);
+
+                Picasso.with(ProfileActivity.this).load(image).into(mProfileImageView);
+
+                mProgress.dismiss();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mProfileSendReqBtn) {
+            if(mCurrent_state.equals("not_friends")){
+                mFriendReqDatabase
+                        .child(mCurrentUser.getUid())
+                        .child(mUserProfileID)
+                        .child("request_type")
+                        .setValue("sent")
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()) {
+
+                            mFriendReqDatabase
+                                    .child(mUserProfileID)
+                                    .child(mCurrentUser.getUid())
+                                    .child("request_type")
+                                    .setValue("received")
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(ProfileActivity.this, "Request Sent Successfully", Toast.LENGTH_LONG).show();
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(ProfileActivity.this, "Failed Sending Request", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        }
     }
 }
