d159b3981b8188f489c6a4a1aa655d9d214ab1d5
Michael Dunlap <michaeldunlap@Michaels-MacBook-Pro.local>
2017-10-21 15:41:16 -0700

    add a device token to Users object on Firebase to enable device-specific notifications

--- app/src/main/java/com/epicodus/classicalchat/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/classicalchat/ui/LoginActivity.java
@@ -14,9 +14,13 @@ import android.widget.Toast;
 
 import com.epicodus.classicalchat.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -32,6 +36,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private ProgressDialog mLoginProgress;
     private FirebaseAuth mAuth;
+    private DatabaseReference mUserDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +51,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         mLoginProgress = new ProgressDialog(this);
         mAuth = FirebaseAuth.getInstance();
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
 
         mLoginBtn.setOnClickListener(this);
     }
@@ -98,10 +104,23 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 if(task.isSuccessful()) {
                     mLoginProgress.dismiss();
 
-                    Intent main_intent = new Intent(LoginActivity.this, MainActivity.class);
-                    main_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(main_intent);
-                    finish();
+                    String current_user_id = mAuth.getCurrentUser().getUid();
+
+                    String deviceToken = FirebaseInstanceId.getInstance().getToken();
+
+                    mUserDatabase.child(current_user_id).child("device_token").setValue(deviceToken).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+
+                            Intent main_intent = new Intent(LoginActivity.this, MainActivity.class);
+                            main_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(main_intent);
+                            finish();
+
+                        }
+                    });
+
+
 
                 } else {
                     mLoginProgress.hide();
